d48495b039f59af479d97a4c785427f64dcb75e1
Dirk Messetat <git@messetat.de>
2021-02-10 09:33:59 +0100

    cleanup

--- ui.apps/frontend/gulpfile.js/index.js
+++ ui.apps/frontend/gulpfile.js/index.js
@@ -1,8 +1,7 @@
 const path = require('path');
 const {src, dest} = require('gulp');
 const gulp = require('gulp');
-const gutil = require('gulp-util');
-
+//const gutil = require('gulp-util');
 
 const source = '../dist';
 const source_resolved = path.resolve(__dirname, source);
@@ -12,8 +11,6 @@ const target = '../../target/jcr_root' + process.env.npm_package_config_target;
 //const target = '../../target/jcr_root' + process.env.npm_config_target ;
 const target_resolved = path.resolve(__dirname, target);
 console.log(`Copy ${source_resolved}/**/* to ${target_resolved}`);
-
-
 
 gulp.task('default', function(cb) {
     src(source_resolved +
