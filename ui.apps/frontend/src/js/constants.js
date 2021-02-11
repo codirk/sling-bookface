7e76d1329adf7acc1430bddd6689ba4bf311665a
Dirk Messetat <git@messetat.de>
2021-02-11 10:15:54 +0100

    messagebus constants removed

--- ui.apps/frontend/src/js/constants.js
+++ ui.apps/frontend/src/js/constants.js
@@ -1,20 +1,4 @@
 define({
     CONSTANT_VALUE: "sample",
-    LOG_LEVEL: 2,
-    CHANNELS:{
-        asset_root:{
-            CHANNEL:'asset_root',
-            topics:{
-                ROOT_CHANGE:'root.change',
-                FILTER_CHANGE:'filter.change'
-            }
-        },
-        asset_item:{
-            CHANNEL:'asset_item',
-            topics:{
-                SIZE_CHANGE:'size.change'
-            }
-        },
-
-    }
+    LOG_LEVEL: 2
 });
